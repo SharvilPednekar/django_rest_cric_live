b3d2f5e5705889c3118eb816aad67a11d11c8bff
Sharvil Pednekar <sharvilpednekar9@gmail.com>
2021-10-21 14:48:09 +0530

    Heroku errors resolved

--- cric_live/settings.py
+++ cric_live/settings.py
@@ -128,7 +128,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
+MEDIA_URL='/media/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+STATIC_ROOT= os.path.join(os.path.dirname(BASE_DIR), 'static_cdn')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
