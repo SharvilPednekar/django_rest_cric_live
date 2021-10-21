8537d840527355c438777f3f4b10630718cc9fee
Sharvil Pednekar <sharvilpednekar9@gmail.com>
2021-10-21 17:03:01 +0530

    static to staticfiles in setings.py

--- cric_live/settings.py
+++ cric_live/settings.py
@@ -139,7 +139,7 @@ STATICFILES_DIRS = (
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
