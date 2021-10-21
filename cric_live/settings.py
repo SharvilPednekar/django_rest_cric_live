040e0b4843401ec96e86ddc7a3e90c13d9976dc7
Sharvil Pednekar <sharvilpednekar9@gmail.com>
2021-10-22 00:34:42 +0530

    Commit

--- cric_live/settings.py
+++ cric_live/settings.py
@@ -139,7 +139,9 @@ STATICFILES_DIRS = (
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT= os.path.join(BASE_DIR, 'static_cdn')
+
 
 
 # Default primary key field type
--- cric_live/urls.py
+++ cric_live/urls.py
@@ -26,4 +26,3 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('', include('display.urls')),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
-  
