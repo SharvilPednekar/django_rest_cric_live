3309fcb1df7f43a08510c135de14ea0ebeeb15ef
Sharvil Pednekar <sharvilpednekar9@gmail.com>
2021-10-21 16:13:58 +0530

    BASE_DIR and STATIC_ROOT changed

--- cric_live/settings.py
+++ cric_live/settings.py
@@ -15,7 +15,7 @@ import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -138,7 +138,7 @@ STATICFILES_DIRS = (
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
-STATIC_ROOT= os.path.join(os.path.dirname(BASE_DIR), 'static_cdn')
+STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
